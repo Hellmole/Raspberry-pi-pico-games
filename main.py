6cd024585ad63cd04a4dabbae3f2af72aeb3dbea
Jakub Jandura <149156309+Hellmole@users.noreply.github.com>
2024-03-26 10:39:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from machine import Pin, I2C, PWM
 from ssd1306 import SSD1306_I2C
 import time
@@ -17,26 +16,24 @@ oled.show()
 time.sleep(2)
 
 
-# game  pong ---------------------------------------------------------------------------------------
+# Game:  Pong ---------------------------------------------------------------------------------------
 
-def hra_pong():
+def game_pong():
 
-    # def variables, buzzer and buttons pins
     x_pos = 2
-    smer = 0
+    direction = 0
     ran = 1
-    smer2 = 1
-    smer3 = 1
+    direction2 = 1
+    direction3 = 1
     x_pos2 = 2
     y_pos2 = 2
     score = 0
     tlacitko = Pin(7, Pin.IN, Pin.PULL_UP)
     tlacitko2 = Pin(15, Pin.IN, Pin.PULL_UP)
     tlacitko3 = Pin(3, Pin.IN, Pin.PULL_UP)
-
     buzzer = PWM(Pin(16))
-    posun = 0
-    zvuk = 0
+    shift = 0
+    sound = 0
 
     while True:
 
@@ -50,90 +47,88 @@ def hra_pong():
         if not tlacitko2.value():  
             x_pos = x_pos - 2 
 
-        # Vymazání displeje aZobrazení Score
+        
         oled.fill(0)  
         oled.text("Score " + str(score), 0, 0)
    
 
-        # Vykreslení palky
+        # bat
         oled.rect(x_pos, 58, 10, 2, 1)
 
-        # Vykresleni mice
+        # ball
         oled.rect(x_pos2 + 5 , 56 - y_pos2, 2, 2, 1)
    
 
-        if posun == 0:
+        if shift == 0:
             x_pos2 = x_pos2 + ran 
-            y_pos2 = y_pos2 + smer3
+            y_pos2 = y_pos2 + direction3
 
     
         if x_pos2 <= 0 or x_pos2 >= 116:  
-            smer2 = -smer2
+            direction2 = -direction2
             ran = -ran
-            zvuk = 1
+            sound = 1
        
         if x_pos <= x_pos2 + 8 and x_pos >= x_pos2 - 8 and y_pos2 == 0:   
-            posun = 0
+            shift = 0
             ran = random.randint(1, 3)
             score = score + 1
-            zvuk = 1
+            sound = 1
 
         elif y_pos2 == 0 :       
             oled.text("GAME OVER", 25, 26)
             oled.show()
             time.sleep(2) 
             x_pos = 2
-            smer = 0
+            direction = 0
             ran = 1
-            smer2 = 1
-            smer3 = 1
+            direction2 = 1
+            direction3 = 1
             x_pos2 = 2
             y_pos2 = 2
             score = 0
    
     
         if y_pos2 <= 0 or y_pos2 >= 54:  
-            smer3 = -smer3
-            zvuk = 1
+            direction3 = -direction3
+            sound = 1
         
 
-        # Zobrazení na displeji
         oled.show()
 
-        # Krátká pauza pro hladší animaci
 
-        if zvuk == 1:
+        if sound == 1:
             buzzer.freq(200)
             buzzer.duty_u16(32768)
             time.sleep(0.02)
             buzzer.duty_u16(0)
-            zvuk = 0
+            sound = 0
             buzzer.deinit()
         else:
             time.sleep(0.02)
 
 
 
-# HRA Modul ---------------------------------------------------------------------------------------
+# Game: Lunar module ---------------------------------------------------------------------------------------
 
-def hra_modul():
+def game_modul():
 
     
     level = 1
     x_pos = 2
-    smer = 0
+    direction = 0
     ran = 1
-    smer2 = 1
-    smer3 = 1
+    direction2 = 1
+    direction3 = 1
     x_pos2 = 2
     y_pos2 = 2
-    ggg= 1
+    gravity= 1
     fuel = 25
-    tryska = 0
+    fire = 0
     
     tlacitko2 = Pin(20, Pin.IN, Pin.PULL_UP)
     tlacitko3 = Pin(3, Pin.IN, Pin.PULL_UP)
-    posun = 0
+    shift = 0
 
     while True:
 
@@ -142,49 +137,47 @@ def hra_modul():
            return
     
         if not tlacitko2.value():
-            tryska = 1
-            ggg = ggg - 5
+            fire = 1
+            gravity = gravity - 5
             fuel = fuel - 1 
 
-    
-    
-        # Vymazání displeje aZobrazení Score
+  
         oled.fill(0)  
         oled.text("Fuel " + str(fuel), 0, 55)
-        oled.text("m/s " + str(ggg), 80, 1)
+        oled.text("m/s " + str(gravity), 80, 1)
     
    
-
-        # Vykreslení modulu
+        # Lunar modul
+        
         oled.rect(6 + x_pos2, 3 + y_pos2, 5, 5, 1)
         oled.vline(5 + x_pos2, 5 + y_pos2, 5, 1)
         oled.vline(11 + x_pos2, 5 + y_pos2, 5, 1)
         oled.rect(7 + x_pos2, 1 + y_pos2, 3, 4 , 1)
 
-        # Vykresleni plochy
+        # landing area
         oled.rect(100, 62, 14, 2, 1)
         
-        if tryska == 1:
+        if fire == 1:
             oled.vline(8 + x_pos2, 11 + y_pos2, 8, 1)
-            tryska = 0
+            fire = 0
 
         x_pos2 = x_pos2 + ran 
-        y_pos2 = y_pos2 + smer3
-        y_pos2 = y_pos2 +  1 + ggg // 10
-        ggg = ggg + 1
+        y_pos2 = y_pos2 + direction3
+        y_pos2 = y_pos2 +  1 + gravity // 10
+        gravity = gravity + 1
        
-        if x_pos2 > 90 and x_pos2 < 110 and y_pos2 >=  56 and ggg < 4:
-            oled.text("Pristani OK", 25, 20)
+        if x_pos2 > 90 and x_pos2 < 110 and y_pos2 >=  56 and gravity < 4:
+            oled.text("Landing OK!", 25, 20)
             level = level + 1
             oled.text("Level " + str(level), 25, 30)
             oled.show()
             time.sleep(2) 
             x_pos = 2
-            smer = 0
-            ggg = 1
+            direction = 0
+            gravity = 1
             ran = ran + 1
-            smer2 = 1
-            smer3 = 1
+            direction2 = 1
+            direction3 = 1
             x_pos2 = 2
             y_pos2 = 2
             fuel = 25
@@ -194,46 +187,38 @@ def hra_modul():
             oled.show()
             time.sleep(2) 
             x_pos = 2
-            smer = 0
-            ggg = 1
+            direction = 0
+            gravity = 1
             ran = 1
-            smer2 = 1
-            smer3 = 1
+            direction2 = 1
+            direction3 = 1
             x_pos2 = 2
             y_pos2 = 2
             fuel = 25
             level = 1
    
 
-        # Zobrazení na displeji
         oled.show()
 
-        # Krátká pauza pro hladší animaci
         time.sleep(0.1)
 
 
-# HRA Moto ---------------------------------------------------------------------------------------
+# Game: Full speed ---------------------------------------------------------------------------------------
 
-def hra_moto():
+def game_moto():
     x = 1
     y = 1
     prekazka = 1
-    smer = 0
     ran = 0
-    smer2 = 1
-    smer3 = 1
+    direction3 = 1
     x_pos = 2
-    qqq = 0
-    naklon = 0
-    y_pos2 = 1
+    tilt = 0
     score = 1
     speed= 1
-    zrychleni = 1
+    acceleration = 1
     level = 1
-    posun = 0
-    y_pos3 = 1
-    y_souper = 0
-    naraz = 0
+    y_rival = 0
+    crasch = 0
 
     tlacitko = Pin(7, Pin.IN, Pin.PULL_UP)
     tlacitko2 = Pin(15, Pin.IN, Pin.PULL_UP)
@@ -244,32 +229,32 @@ def hra_moto():
     while True:
     
         oled.fill(0) 
-        naklon = 0
+        tilt = 0
         if not tlacitko3.value():  
            return
 
         if not tlacitko.value():  
             x_pos = x_pos + 2
-            naklon = 4 
+            tilt = 4 
   
         if not tlacitko2.value():  
             x_pos = x_pos - 2
-            naklon = -4
+            tilt = -4
     
-        # Vymazání displeje aZobrazení Score
-        y_souper = ran + y
+        y_rival = ran + y
         oled.text("Score:" + str(score), 0, 0)
         oled.text(str(score * 5) + " km/h", 70, 0)
     
         x = x + 1
-        y = y + smer3
+        y = y + direction3
         prekazka = prekazka + speed
 
     
-        # Vykreslení hor
+        # horizont
         oled.line(20 + y // 5, 35 + y // 10 , 9 + y // 4 , 39 + y // 10, 1)
         oled.line(20 + y // 5, 35 + y // 10, 29 + y // 4, 39 + y // 10, 1)
-        # Vykreslení silnice
+        
+        # road
     
     
         oled.rect(0, 40 + y //10, 128, 2, 1)
@@ -284,13 +269,13 @@ def hra_moto():
     
         oled.rect(0, 52 + x, 128, 8, 0)
     
-        # Vykreslení motorky
+        # your moto
         oled.rect(60 + x_pos, 58, 2, 4, 1)
-        oled.rect(59  + x_pos + naklon // 2, 55, 5, 4, 1)
+        oled.rect(59  + x_pos + tilt // 2, 55, 5, 4, 1)
 
-        oled.rect(60 + x_pos + naklon, 52, 2, 2, 1)
+        oled.rect(60 + x_pos + tilt, 52, 2, 2, 1)
 
-        # prekazka
+        # rival
         if prekazka > 10: 
             oled.rect(60 + ran + y, 38 + prekazka , 2, 4, 1)
             oled.rect(59 + y // 20  + ran + y, 35 + prekazka, 5, 4, 1)
@@ -310,12 +295,12 @@ def hra_moto():
             ran = ran * 2
             prekazka = 0
             score = score + 1
-            zrychleni = zrychleni + 0.05
-            speed = round(zrychleni)
+            acceleration = acceleration + 0.05
+            speed = round(acceleration)
     
 
         if y <= -35  or y >= 25:  
-            smer3 = -smer3
+            direction3 = -direction3
 
         if y <= -15:  
             x_pos = x_pos + 2
@@ -324,36 +309,29 @@ def hra_moto():
             x_pos = x_pos - 2
             
         if x_pos >=  46 or x_pos < -46:       
-            naraz = 1
+            crasch = 1
 
-        if x_pos <= y_souper + 4  and x_pos >= y_souper - 4 and prekazka >= 15:       
-            naraz = 1
+        if x_pos <= y_rival + 4  and x_pos >= y_rival - 4 and prekazka >= 15:       
+            crasch = 1
 
 
-        if  naraz ==  1:  
-
+        if  crasch ==  1:  
             oled.text("GAME OVER", 25, 26)
             oled.show()
             time.sleep(2)
             x = 1
             y = 1
             prekazka = 1
-            smer = 0
             ran = 0
-            smer2 = 1
-            smer3 = 1
+            direction3 = 1
             x_pos = 2
-            qqq = 0
-            naklon = 0
-            y_pos2 = 1
+            tilt = 0
             score = 1
             speed= 1
-            zrychleni = 1
+            acceleration = 1
             level = 1
-            posun = 0
-            y_pos3 = 1
-            y_souper = 0
-            naraz = 0
+            y_rival = 0
+            crasch = 0
 
         time.sleep(0.1)  
 
@@ -365,21 +343,21 @@ def hlavni_menu():
     while True:
          
         oled.fill(0)  
-        oled.text("Pong v1.0", 25, 26)
+        oled.text("   Pong  ", 18, 25)
         oled.show()
         time.sleep(2)
-        hra_pong() 
+        game_pong() 
 
         oled.fill(0)  
-        oled.text("Modul v1.0", 25, 26)
+        oled.text("Lunar module", 18, 25)
         oled.show()
         time.sleep(2)
-        hra_modul()         
+        game_modul()         
 
         oled.fill(0)  
-        oled.text("Moto v1.0", 25, 26)
+        oled.text(" Full speed", 18, 25)
         oled.show()
         time.sleep(2)
-        hra_moto()      
+        game_moto()      
 hlavni_menu()
 
